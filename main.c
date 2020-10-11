ad1a5c93abcdf79d2dff2a72f1646afff9b63f72
RECEIVERchris <hongcsw3928@gmail.com>
2020-10-11 12:41:17 +0000

    1

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+
+int add(int a, int b)
+{
+  return a + b;
+
+}
+
+
+
+int main()
+
+{ 
+
+  
+  int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+
+
+  printf("data: \n");
+  scanf("%d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10 );
+
+  int sum;
+
+  sum = add();
+
+  printf("sum: ");
+  
+
+  return 0;
+
+
+
+}
